177fe6c55cf29c1040a92f9e5be6aaa34dbc42fc
chri9663 <chris.mead@rackspace.com>
2018-06-19 17:00:07 -0600

    specify package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=REQUIRES,
     tests_require=TESTS_REQUIRES,
     extras_require=EXTRAS,
-    packages=[],
+    packages=['seleniumpool'],
     include_package_data=False,
     long_description="""\
     Python library for concurrent selenium testing
