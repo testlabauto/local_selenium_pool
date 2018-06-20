31cc5467e48600aaf4ee984714cd5b2bc9dac5d0
chri9663 <chris.mead@rackspace.com>
2018-06-20 09:10:32 -0600

    creating egg

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="local_selenium_pool",
-    version="1.0",
+    version="0.0.1",
     description="Concurrent local selenium execution using multiprocessing_on_dill",
     author_email="chris@testlabauto.com",
     author="Test Lab Automation",
     license="Apache License Version 2.0",
     url="https://github.com/testlabauto/local_selenium_pool",
-    keywords=["Selenium", "Parallel", "Concurrent"],
+    keywords=["Selenium", "Local", "Concurrent"],
     install_requires=['selenium', 'attr', 'setuptools', 'multiprocessing_on_dill'],
     packages=find_packages(),
-    zip_safe=False,
-    long_description="""\
-    Python library for concurrent selenium testing
-    """,
     classifiers=[
         "Development Status :: 1 - Beta",
         "Topic :: Utilities",
