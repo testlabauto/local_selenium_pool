de495117b1f7959eb1561a7f4d711cbc58c36c54
chri9663 <chris.mead@rackspace.com>
2018-06-22 17:16:47 -0600

    packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="local_selenium_pool",
-    version="1.0.3",
+    version="1.0.7",
     description="Concurrent local selenium execution using multiprocessing_on_dill",
     long_description="A local selenium pool for increased testing performance without requiring multiple hosts. multiprocessing-on-dill is used to provide a configurable number of Chrome webdriver instances on which to simultaneously run selenium tests. Each instance reuses its applicationCacheEnabled = False webdriver for multiple tests, erasing all cookies between tests.  After the pool of webdrivers has no remaining tests to execute, it creates a JSON report in an XUnit style.",
     author_email="chris@testlabauto.com",
@@ -12,7 +12,8 @@ setup(
     url="https://github.com/testlabauto/local_selenium_pool",
     keywords=["Selenium", "Local", "Concurrent"],
     install_requires=['selenium', 'attr', 'setuptools', 'multiprocessing_on_dill'],
-    packages=find_packages(),
+    packages=['local_selenium_pool'],
+    include_package_data=True,
     zip_safe=False,
     classifiers=[
         "Development Status :: 3 - Alpha",
