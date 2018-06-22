c031748f9e5c8b5199b6740bb3999c08699ebb7f
chri9663 <chris.mead@rackspace.com>
2018-06-22 14:41:03 -0600

    license.txt added

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="local_selenium_pool",
     version="0.0.1",
     description="Concurrent local selenium execution using multiprocessing_on_dill",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email="chris@testlabauto.com",
     author="Test Lab Automation",
     license="Apache License Version 2.0",
